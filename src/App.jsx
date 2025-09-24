bcac316eddfc05c3b6a4859de5dffa5d7ca8c2e2
maferr10 <marsaenzcastro@crackthecode.la>
2025-09-24 12:12:54 -0500

    diasfrutasanimales

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ListaAnimales } from './componentes/ListaAnimales'
+import { Frutas } from './componentes/Frutas'
+import { Diasdelasemana } from './componentes/Diasdelasemana'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const animales = [{nombre:"gato", imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/250px-Cat_November_2010-1a.jpg"},
+                    {nombre:"perro", imagen:"https://eq2imhfmrcc.exactdn.com/wp-content/uploads/2016/08/golden-retriever.jpg?strip=all&lossy=1&ssl=1"},
+                    {nombre:"lobo", imagen:"https://static.nationalgeographicla.com/files/styles/image_3200/public/05_nationalgeographic_2711495.webp?w=839&h=839"}
+                  ] 
+  const frutas = ["manzana","pera","uva"]
+  const dias = [ "lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>Lista de Animales</h1>
+    {animales.map(function(animal){
+
+    return<ListaAnimales imagen={animal.imagen} nombre={animal.nombre} />
+
+  })}
+  <h2>Lista de Frutas</h2>
+  <ul>{
+    frutas.map(function(fruta){
+      return<Frutas nombre={fruta} />
+    })
+    }
+
+  </ul>
+  <h2>Lista de dias de la semana</h2>
+  <ul>{
+    dias.map(function(dias){
+      return <Diasdelasemana nombre={dias}/>
+    })
+    }</ul>
+
+  </>
   )
 }
 
---
+++ src/componentes/Diasdelasemana.jsx
@@ -0,0 +1,5 @@
+export function Diasdelasemana ({nombre}) {
+    return(
+        <li>{nombre}</li>
+    )
+}
